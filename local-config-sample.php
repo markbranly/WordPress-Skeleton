0c4a68377357a8589aa2004a553ebcce7bc8c60c
Mark Branly <mark@registeredcreative.com>
2013-08-27 13:12:28 -0400

    Deleted roots. Updated local-config-sample.php.

--- local-config-sample.php
+++ local-config-sample.php
@@ -12,6 +12,10 @@ define( 'DB_PASSWORD', 'local_db_password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
 
 
+// define('WP_SITEURL', 'http://10.99.9.20/subdir/wp');
+// define('WP_HOME', 'http://10.99.9.20/subdir');
+// define('RELOCATE', true);
+
 // ================================================
 // Custom Content Directory
 // If you have a different url scheme for locat dev
@@ -23,5 +27,10 @@ define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
 // Debug mode
 // Always enable these in local-config.php
 // =================================================================
+@ini_set('log_errors','Off');
+@ini_set('display_errors','On');
+@ini_set('error_reporting', E_ALL );
 define( 'SAVEQUERIES', true );
 define( 'WP_DEBUG', true );
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
